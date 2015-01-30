0d1197a0fa90e9868b72f91aabc066bf4c80b1d6
Christiano Milfont <cmilfont@gmail.com>
2015-01-30 18:44:01 -0300

    incluindo rota para logar

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -9,4 +9,15 @@ class LogsController < ApplicationController
     
   end
   
+  def create
+    @log = Log.create user_id: current_user.id,
+                      kind: params[:kind],
+                      description: params[:description], 
+                      logged_at: params[:logged_at]
+    
+#     Log.create user_id:1, kind: Log::HUNGRY, description: "#soft", logged_at: "2015-01-30 15:24:00"
+    
+    respond_with @log
+  end
+  
 end
