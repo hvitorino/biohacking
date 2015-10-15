d7fa78e0afcc2e2405e66d9c2e0c0cd080eb0c9d
Christiano Milfont <cmilfont@gmail.com>
2015-10-15 20:17:47 -0300

    nego ta dificil

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( moment-with-locales.min.js angular.min.js angular-route.min.js angular-resource.min.js angular-sanitize.min.js ")
