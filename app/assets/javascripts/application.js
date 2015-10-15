d8ae68cf7cb056acef68d4d34a65f5218966f6db
Christiano Milfont <cmilfont@gmail.com>
2015-10-15 20:03:11 -0300

    agora vai

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,10 @@
 //
-//= require djr
-//= require biohacking/packages
-//= require biohacking/EventListener
-//= require biohacking/form/fields/Field
-//= require biohacking/form/fields/Text
-//= require biohacking/form/fields/Button
-//= require biohacking/form/fields/Date
-//= require biohacking/form/fields/Display
-//= require biohacking/form/fields/LookupOption
-//= require biohacking/form/fields/Lookup
-//= require biohacking/form/Section
-//= require biohacking/form/Builder
-//= require biohacking/form/LogCreate
-//= require biohacking/messages
-//= require app
+//= require moment-with-locales.min.js
+//= require angularjs/angular.min.js
+//= require angularjs/angular-route.min.js
+//= require angularjs/angular-resource.min.js
+//= require angularjs/angular-sanitize.min.js
+//= require angularjs/app
+//= require angularjs/models/LogModel.js
+//= require angularjs/services/LogsServices.js
+//= require angularjs/controllers/LogsController.js
