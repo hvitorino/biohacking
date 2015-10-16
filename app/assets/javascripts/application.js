3990cfbf91e29c59398041987a6346c05869c748
Christiano Milfont <cmilfont@gmail.com>
2015-10-16 07:45:02 -0300

    Bootstrap manual

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,7 @@
 //= require angularjs/app
 //= require angularjs/services/LogsServices
 //= require angularjs/controllers/LogsController
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['Biohacking']);
+});
