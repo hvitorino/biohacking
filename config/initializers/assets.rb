d07b507c5cb9d96ed970204b72a09c4fc39114f2
Christiano Milfont <cmilfont@gmail.com>
2015-10-16 13:26:28 -0300

    sem digest e compile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( moment-with-locales.min.js)
+Rails.application.config.assets.compile = false
+Rails.application.config.assets.digest = false
