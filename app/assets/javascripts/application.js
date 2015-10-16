62e245eef0bc39dc683a58be3705294222051189
Christiano Milfont <cmilfont@gmail.com>
2015-10-16 07:50:10 -0300

    voltando o local

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,8 @@
 //
+//= require angular.min.js
+//= require angular-resource.min.js
+//= require angular-route.min.js
+//= require angular-sanitize.min.js
 //= require angularjs/app
 //= require angularjs/services/LogsServices
 //= require angularjs/controllers/LogsController
