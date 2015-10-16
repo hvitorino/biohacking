77b10743512b2f86a83c15981248c1fee67c943e
Christiano Milfont <cmilfont@gmail.com>
2015-10-16 12:27:19 -0300

    sem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,3 @@
 //= require angular-resource.min.js
 //= require angular-route.min.js
 //= require angular-sanitize.min.js
-//= require angularjs/app
-//= require angularjs/services/LogsServices
-//= require angularjs/controllers/LogsController
