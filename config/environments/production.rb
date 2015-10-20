e203ff81b2af747656b15b37bff21bc2b3795c3e
Christiano Milfont <cmilfont@gmail.com>
2015-10-20 18:49:46 -0300

    Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.action_mailer.default_url_options = { :host => 'biohacking.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+            :address              => "smtp.gmail.com",
+            :port                 => 587,
+            :domain               => "milfont.org",
+            :user_name            => "teste@milfont.org",
+            :password             => "147/258*369",
+            :authentication       => "plain",
+            :enable_starttls_auto => true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
