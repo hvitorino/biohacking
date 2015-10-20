726e631f5856f56951ea239c7a79f051a7e317cb
Christiano Milfont <cmilfont@gmail.com>
2015-10-20 19:25:46 -0300

    tentando

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  config.action_mailer.default_url_options = { :host => 'biohacking.herokuapp.com/', :protocol: 'https' }
+  config.action_mailer.default_url_options = { :host => 'biohacking.herokuapp.com/', :protocol => 'https' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -14,6 +14,7 @@ Rails.application.configure do
             :authentication       => "plain",
             :enable_starttls_auto => true
   }
+  config.action_mailer.raise_delivery_errors = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
