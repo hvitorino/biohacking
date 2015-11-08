587e5a3368dabf6291c19a30dff4704766427ac8
Christiano Milfont <cmilfont@gmail.com>
2015-11-08 18:37:53 -0300

    bobagem

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,13 +3,16 @@ class ChartsController < ApplicationController
   respond_to :json
   
   def index
+  
+    #@serieA = Log.where(user_id: 1, kind: Log::WEIGHT).select{|log| log.description.present? }
+    #@serieB = Log.where(user_id: 1, kind: Log::DRINK).select{|log| log.description.present? }
     
     @chart = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      series: ['Series A', 'Series B'],
+      labels: ["2015-11-01", "2015-11-02", "2015-11-06", "2015-11-08"],
+      series: ['WEIGHT', 'DRINK #coffee'],
       data:  [
-         [65, 59, 80, 81, 56, 55, 40],
-         [28, 48, 40, 19, 86, 27, 90]
+         [82.5, 0, 85, 83],
+         [250, 250, 500, 500]
       ]
     }
     
