20fef940f7d6e7f90785dd185d8bf3b510656a79
Levy Moreira <me@levymoreira.com>
2016-11-12 11:42:43 -0300

    Add reducer to activities update success

--- client/src/api/reducers/activities.js
+++ client/src/api/reducers/activities.js
@@ -4,5 +4,11 @@ export default (state = [], { type, payload }) => {
   if (type === actions.activities.requestSucess) {
     return payload;
   }
+
+  if (type === actions.activities.updateSuccess) {
+    console.log('Reducer update success!');
+    return payload;
+  }
+
   return state;
 }
