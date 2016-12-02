76932b4b29a5679787eb319b0fb769be20ff35f1
Christiano Milfont <cmilfont@gmail.com>
2016-12-01 21:17:26 -0300

    [#master] Falta implementar a saga da navegação

--- client/src/Base.jsx
+++ client/src/Base.jsx
@@ -1,23 +1,50 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Hammer from 'react-hammerjs';
 import Menu from 'Menu.jsx';
 
 class Base extends React.Component {
 
   static propTypes = {
     children: PropTypes.element,
+    onSwipeLeft:  PropTypes.func,
+    onSwipeRight:  PropTypes.func,
   }
 
+  // onSwipeLeft = () => {
+  //   if (this.props.location.pathname === '/activities') {
+  //     this.context.router.push('/new');
+  //   } else if (this.props.location.pathname === '/search') {
+  //     this.context.router.push('/activities');
+  //   }
+  // }
+  //
+  // onSwipeRight = () => {
+  //   if (this.props.location.pathname === '/new') {
+  //     this.context.router.push('/activities');
+  //   } else if (this.props.location.pathname === '/activities') {
+  //     this.context.router.push('/search');
+  //   }
+  // }
+
   render () {
-    const { children } = this.props;
+    const { children, onSwipeLeft, onSwipeRight } = this.props;
     return (
-      <div className="mdl-layout mdl-layout--fixed-header">
-        <Menu />
-        <main className="mdl-layout__content">
-          {children}
-        </main>
-      </div>
+      <Hammer onSwipeLeft={onSwipeLeft} onSwipeRight={onSwipeRight}>
+        <div className="mdl-layout mdl-layout--fixed-header">
+          <Menu />
+          <main className="mdl-layout__content">
+            {children}
+          </main>
+        </div>
+      </Hammer>
     )
   }
 }
 
-export default Base;
+export default connect(null, (dispatch) => {
+  return {
+    onSwipeLeft: () => dispatch({ type: 'GO_LEFT' }),
+    onSwipeRight: () => dispatch({ type: 'GO_RIGHT' }),
+  }
+})(Base);
